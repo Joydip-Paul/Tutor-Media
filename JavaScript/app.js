e652cf0ddd3181155a41aca296bb0ee9e5c7db19
Joydip Paul <joydippaul72@gmail.com>
2021-05-25 00:09:32 +0600

    login modal

---
+++ JavaScript/app.js
@@ -0,0 +1,9 @@
+const close = document.getElementById("closeDiv");
+
+const closed = () => {
+  close.style.display = "none";
+};
+
+const signInClick = () => {
+  close.style.display = "block";
+};
